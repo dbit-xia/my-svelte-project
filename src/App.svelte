ffc808ca2f835502b96090b9189445f46f36b12d
derek@runsasoft.com <derek@runsasoft.com>
2021-01-24 01:03:52 +0800

    initial

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+    import Test02 from './Test02.svelte';
+	export let name;
+	export let visible=false;
+	const handleClick=()=>{
+		name=123;
+		visible=true;
+	}
+
+	
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={handleClick}>OK</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+{#if visible}
+	<Test02 name={name}/>
+{/if}
